da88cdd678362aea22d46540b9a7c4c4eeb936d7
Jan Lewiński <janmikolajlewinski@gmail.com>
2026-06-08 20:10:51 +0200

    localhost server via mux

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, I'm Chirpy!")
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
